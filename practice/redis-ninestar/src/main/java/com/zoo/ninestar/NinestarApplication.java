f4cdcab3eefbeadfef09c93ed559fb56b84958b0
liuxun  <liuxun@cyou-inc.com>
2021-07-12 10:25:47 +0800

    [九星神器] modify application name

--- practice/redis-ninestar/src/main/java/com/zoo/ninestar/NinestarApplication.java
+++ practice/redis-ninestar/src/main/java/com/zoo/ninestar/NinestarApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZooNinestarApplication {
+public class NinestarApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZooNinestarApplication.class, args);
+        SpringApplication.run(NinestarApplication.class, args);
     }
 
 }
